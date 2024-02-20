af820408cabff9167cd5a3d3ae1eee265eb100d1
Mostafa <83466461+mostafa513660@users.noreply.github.com>
2024-02-21 01:56:43 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def prices():
+@app.route('/prices')
+def page():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
