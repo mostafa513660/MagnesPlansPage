1bbbf450cfd515b3b630c2b61147cdc6e7444f36
Mostafa <83466461+mostafa513660@users.noreply.github.com>
2024-02-21 00:34:40 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/prices')
-def page():
+def prices():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='localhost')
+    app.run(host='0.0.0.0', port=5000)
